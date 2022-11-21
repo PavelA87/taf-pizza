fe5990edf87123a74f6da36aa0f9418d11645baf
Pavel Aponik <aponikpavel@gmail.com>
2022-11-21 19:13:55 +0300

    refactor

--- test/java/by/itacademy/aponikpavel/tafpizza/TerrapizzaTest.java
+++ test/java/by/itacademy/aponikpavel/tafpizza/TerrapizzaTest.java
@@ -13,17 +13,24 @@ public class TerrapizzaTest {
     public void testOpenTerrapizzaPage(){
 
         String url = "https://terrapizza.by/";
-        String expected = "Пицца Маргарита";
+        String expected = "Пицца Маргарита Классическая 32 см";
         String xPathButtonMenu = "//li[@class ='main-menu__item']/a[@href='https://terrapizza.by/menu']";
         String xPathButtonPizza = "//div[17]/a[@href='/catalog/picca']";
         String xPathButtonPizzaMargaritaAddInOrder = "//button[@data-id='364']";
-        String xPathButtonOrder = "//div[@class = 'shopping-cart-btn page-cap__shopping-cart-btn']";
+        String xPathButtonOrder = "//div[@class='shopping-cart-btn page-cap__shopping-cart-btn ']";
         String xPathPizzaMargaritaInOrder = "//div[@class='basket__products-item-name']";
         String xPathNumberOfPizzaMargaritaInOrder = "//input[@class='basket__counter-input inp-count-el-0']";
+        String xPathCloseCookies = "//button[@class='close-icon']";
 
         WebDriverManager.chromedriver().setup();
         ChromeDriver driver = new ChromeDriver();
         driver.get(url);
+        driver.manage().window().maximize();
+
+        By byCloseCookies = By.xpath(xPathCloseCookies);
+        WebElement webElementCloseCookies = driver.findElement(byCloseCookies);
+        webElementCloseCookies.click();
+
 
         By byButtonMenu = By.xpath(xPathButtonMenu);
         WebElement webElementButtonMenu = driver.findElement(byButtonMenu);
@@ -37,13 +44,15 @@ public class TerrapizzaTest {
         WebElement webElementButtonPizzaMargaritaAddInOrder = driver.findElement(byButtonPizzaMargaritaAddInOrder);
         webElementButtonPizzaMargaritaAddInOrder.click();
 
+        driver.get(url);
+
         By byButtonOrder = By.xpath(xPathButtonOrder);
         WebElement webElementButtonOrder = driver.findElement(byButtonOrder);
         webElementButtonOrder.click();
 
         By byPizzaMargaritaInOrder = By.xpath(xPathPizzaMargaritaInOrder);
         WebElement webElementPizzaMargaritaInOrder = driver.findElement(byPizzaMargaritaInOrder);
-        String actual = webElementButtonPizzaMargaritaAddInOrder.getText();
+        String actual = webElementPizzaMargaritaInOrder.getText();
 
         Assertions.assertEquals(expected, actual);
         driver.quit();
